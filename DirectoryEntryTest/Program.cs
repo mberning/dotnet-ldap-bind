04114db5a9f9e46070afc83d50b26e2f56718518
meb <mberning@gmail.com>
2024-09-11 13:02:48 -0400

    Load config from json file

--- DirectoryEntryTest/Program.cs
+++ DirectoryEntryTest/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.DirectoryServices;
+using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+String configFileText = File.ReadAllText("config.json");
+Config config = JsonSerializer.Deserialize<Config>(configFileText);
+
+String ldapUrl = config.ldapUrl;
+String ldapUser = config.ldapUser;
+String ldapPassword = config.ldapPassword;
+AuthenticationTypes ldapAuthType = AuthenticationTypes.Encryption | AuthenticationTypes.ServerBind;
+
+DirectoryEntry entry = new DirectoryEntry(ldapUrl, ldapUser, ldapPassword, ldapAuthType);
+Console.WriteLine(entry.Parent.Path);
+Console.WriteLine("Done");
+
+public class Config
+{
+    public String ldapUrl;
+    public String ldapUser;
+    public String ldapPassword;
+}
